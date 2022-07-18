33b8837b9643e55c8cdfef59353b02a5626e3425
Muzna Moazzam <109554185+MuznaMoazzam@users.noreply.github.com>
2022-07-18 17:04:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.title ('Breakfast Menu')
+streamlit.header ('Breakfast Menu')
+streamlit.text('Omega 3 and blueberry oatmeal')
+streamlit.text('Kale,spinach and rocket smoothie')
+streamlit.text('Hard-Boiled Free range egg')
