c883856344b9d29c188e29e02b422efce280aa15
Muzna Moazzam <109554185+MuznaMoazzam@users.noreply.github.com>
2022-08-04 14:20:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header ('Breakfast Favourites')
 streamlit.text(' 🥣 Omega 3 and blueberry oatmeal')
@@ -7,7 +12,7 @@ streamlit.text('🐔 Hard-Boiled Free range egg')
 streamlit.text('🥑🍞 Avacado toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import pandas
+
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -22,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 # streamlit.text(fruityvice_response.json())
 
@@ -30,7 +35,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output on the screen as a table
 streamlit.dataframe(fruityvice_normalized)
-import snowflake.connector
+
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -46,3 +51,12 @@ streamlit.write('Thanks for adding', add_my_fruit)
 
 #This will not work but just with it for now
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+#don't run anything past here until we troubleshoot
+streamlit.stop();
+my_cnx=snowflake.connector.connect(**streamlit.secrets["Snowflake"])
+my_cur=my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list ")
+my_data_rows=my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe=(my_data_rows)
